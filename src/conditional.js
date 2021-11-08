b227cec41791855b2150c88d2802512faeb70f6d
Natalia Huberna <natalyahuberna@gmail.com>
2021-11-08 23:48:08 +0200

    rewriting expression to function

--- src/conditional.js
+++ src/conditional.js
@@ -1,85 +1,100 @@
 // 1.	Если а – четное посчитать а*б, иначе а+б
-const a = 8;
-const b = 5;
-if (a % 2) {
-    console.log(a * b); 
-} else {
-    console.log(a + b); 
+
+function getResult(a, b) {
+    return (a % 2
+        ? a + b
+        : a * b);
 }
+
+console.log('[getResult(4, 3)]', getResult(4, 3));
+
 
 // 2.	Определить какой четверти принадлежит точка с координатами (х,у)
-const x = 0;
-const y = -2;
-switch (true) {
-    case (x > 0 && y > 0):
-        console.log('The first quarter');
-        break;
-    case (x < 0 && y > 0):
-        console.log('The second quarter');
-        break;
-    case (x < 0 && y < 0):
-        console.log('The third quarter');
-        break;
-    case (x > 0 && y < 0):
-        console.log('The fourth quarter');
-        break;
+
+function defineQuater(x, y) {
+    let answer = "";
+  switch (true) {
+    case x > 0 && y > 0:
+      answer = "The first quarter";
+    break;
+    case x < 0 && y > 0:
+      answer = "The second quarter";
+    break;
+    case x < 0 && y < 0:
+      answer = "The third quarter";
+    break;
+    case x > 0 && y < 0:
+      answer = "The fourth quarter";
+    break;
     default:
-        console.log('The point lies on the coordinate line');
-        break;
+      answer = "The point lies on the coordinate line";
+    break;
+    }
+    return answer;
 }
+
+console.log('[defineQuater (-1, 3)]', defineQuater (-1, 3));
 
 // 3.	Найти суммы только положительных из трех чисел
-const d = -5;
-const e = 3;
-const f = 1;
+
+function getSum (d, e, f) {
 let sum = 0;
-switch (true) {
-    case d > 0:
+    if (d > 0) {
         sum += d;
-        console.log(sum);
-    case e > 0:
+    }
+      
+    if (e > 0) {
         sum += e;
-        console.log(sum);
-    case f > 0:
+    }
+       
+    if (f > 0) {
         sum += f;
-        console.log(sum);
-    default:
-        break;
+    }
+    
+    return sum;
 }
+
+console.log('[ getSum (-2, -3, 4)]', getSum(-2, -3, 4));
 
 // 4.	Посчитать выражение (макс(а*б*с, а+б+с))+3
-const g = 5;
-const h = 15;
-const i = 7;
+
+function countMaxExpression(g, h, i) {
 let mult = g * h * i;
 let add = g + h + i;
-if (mult - add) {
-    console.log(mult += 3);
-} else {
-   console.log(add += 3);
+    return (mult > add
+        ? mult += 3
+        : add += 3);
 }
 
-// 5.	Написать программу определения оценки студента по его рейтингу, на основе следующих правил
-const rating = 100;
-switch (true) {
-  case (0 <= rating && rating <= 19):
-    console.log("Mark F");
-    break;
-  case (20 <= rating && rating <= 39):
-    console.log("Mark E");
-    break;
-  case (40 <= rating && rating <= 59):
-    console.log("Mark D");
-    break;
-  case (60 <= rating && rating <= 74):
-    console.log("Mark C");
-    break;
-  case (75 <= rating && rating <= 89):
-    console.log("Mark B");
-    break;
-  case (90 <= rating && rating <= 100):
-    console.log("Mark A");
-        break;
-  default:
-        break;
+console.log('[countMaxExpression (1, 2, 3)]', countMaxExpression(1, 2, 3));
+
+// 5.	Написать программу определения оценки студента по его рейтингу, на основе следующих 
+function getMark(rating) {
+    let mark;
+  
+    switch (true) {
+    case 0 <= rating && rating <= 19:
+      mark = "Mark F";
+      break;
+    case 20 <= rating && rating <= 39:
+      mark = "Mark E";
+      break;
+    case 40 <= rating && rating <= 59:
+      mark = "Mark D";
+      break;
+    case 60 <= rating && rating <= 74:
+      mark = "Mark C";
+      break;
+    case 75 <= rating && rating <= 89:
+      mark = "Mark B";
+      break;
+    case 90 <= rating && rating <= 100:
+      mark = "Mark A";
+      break;
+    default:
+      break;
+    }
+    return mark;
 }
+
+console.log('[getMark(100)]', getMark(100));
